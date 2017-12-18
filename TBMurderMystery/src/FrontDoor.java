59fdfbba88e90659f496dfdffdff3007c25232b0
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-12-18 09:11:13 -0500

    ddzxgwsrg

--- TBMurderMystery/src/FrontDoor.java
+++ TBMurderMystery/src/FrontDoor.java
@@ -1,4 +1,24 @@
 
-public class FrontDoor {
-
+public class FrontDoor extends Room
+{
+	Person occupant;
+	int xLoc, yLoc;
+	
+	public FrontDoor (int x, int y)
+	{
+		xLoc = 5;
+		yLoc = 2;
+	}
+	
+	public void enterRoom (Person x)
+	{
+		occupant = x;
+		x.setxLoc(this.xLoc);
+		x.setyLoc(this.yLoc);
+	}
+	
+	public void leaveRoom (Person x)
+	{
+		occupant = null;
+	}
 }
