99c7faa41c2af23440e19ce77917734e9bbd79e4
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-12-19 09:27:22 -0500

    asdfghjkhgfdsasdf What am i Doing Help me TM

--- TBMurderMystery/src/DiningRoom.java
+++ TBMurderMystery/src/DiningRoom.java
@@ -1,4 +1,25 @@
 
-public class DiningRoom {
-
+public class DiningRoom extends Room
+{
+	Person occupant;
+	int xLoc, yLoc;
+	
+	public DiningRoom (int x, int y)
+	{
+		//how to add more than one room as Dining Room ??
+		xLoc = 2;
+		yLoc = 0;
+	}
+	
+	public void enterRoom (Person x)
+	{
+		occupant = x;
+		x.setxLoc(this.xLoc);
+		x.setyLoc(this.yLoc);
+	}
+	
+	public void leaveRoom (Person x)
+	{
+		occupant = null;
+	}
 }
